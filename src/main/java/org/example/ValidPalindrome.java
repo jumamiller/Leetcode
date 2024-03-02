0424c6b4f8e8276d76e81090c843daa1198ea2d6
Jakabos <jumamiller@yahoo.com>
2024-03-02 12:44:07 +0300

    valid palidntrom

--- src/main/java/org/example/ValidPalindrome.java
+++ src/main/java/org/example/ValidPalindrome.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class ValidPalindrome {
     public boolean checkValidPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-z]","");
+        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
         //
         int i=0;
         int j=s.length()-1;
