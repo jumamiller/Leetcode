1f8e50dfb75f2045c2e507d84509542f1ffe8ec2
Jakabos <jumamiller@yahoo.com>
2024-03-02 12:43:24 +0300

    valid palidntrom

--- src/main/java/org/example/ValidPalindrome.java
+++ src/main/java/org/example/ValidPalindrome.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class ValidPalindrome {
     public boolean checkValidPalindrome(String s) {
+        s=s.toLowerCase().replaceAll("[^a-z]","");
+        //
+        int i=0;
+        int j=s.length()-1;
 
+        while(i<j) {
+            if (s.charAt(i)==s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
